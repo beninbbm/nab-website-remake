93fecd6bda57598169cd101981537e63312e5e16
beninbmathew <beninbbm@gmail.com>
2022-06-23 14:25:09 +1000

    Majority of header complete

--- nab-website-remake/src/components/Navbar.js
+++ nab-website-remake/src/components/Navbar.js
@@ -1,16 +1,34 @@
 import './Navbar.css';
 
 const Navbar = () => {
-
-  console.log(window.innerWidth);
   return (
     <div className='Navbar'>
-      <img className='NABLogo' src={require('../images/nab-logo.jpg')} alt='NAB Logo'/>
+      <img className='NABLogo' src={require('../images/nab-logo.jpg')} alt='NAB logo'/>
       <div className='NavBarButtons'>
         <a href="#Personal">Personal</a>
         <a href="#Business">Business</a>
         <a href="#Corporate">Corporate</a>
         <a href="#About Us">About Us</a>
+      </div>
+
+      <div className="HeaderRightSide">
+        <img className='MagnifyingGlass' src={require('../images/magnifying-glass.jpg')} alt='White magnifying glass'/>
+        <img className='QuestionMark' src={require('../images/question-mark.jpg')} alt='White question mark'/>
+        <div className="HeaderRightSideDropDown">
+        <div className="HeaderRightSideDropDownButtonAndArrowAndButton">
+          <button class="HeaderRightSideDropDownButton">Internet Banking</button>
+          <img className='WhiteDownArrow' src={require('../images/white-down-arrow.png')} alt='White down arrow'/>
+          <button class="HeaderRightSideLoginButton">Login</button>
+        </div>
+          <div className="HeaderRightSideDropDownContent">
+            <a href="#InternetBanking">Internet Banking</a>
+            <a href="#NABConnect">NAB Connect</a>
+            <a href="#nabtrade">nabtrade</a>
+            <a href="#MarginLending">Margin Lending</a>
+            <a href="#NABTransact">NAB Transact</a>
+            <a href="#AssetServicing">Asset Servicing</a>
+          </div>
+        </div>
       </div>
     </div>
   );
